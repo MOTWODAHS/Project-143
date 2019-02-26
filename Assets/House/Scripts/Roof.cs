b42437f26bced81632aa6ff38eaecf0fa08a6f2b
Qianye(Renee) Mei <meiqianye@gmail.com>
2019-02-26 14:30:43 -0500

    add house projection project

---
+++ Assets/House/Scripts/Roof.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using TouchScript.Behaviors;
+using TouchScript.Gestures.TransformGestures;
+
+public class Roof : MonoBehaviour
+{
+    private TransformGesture gesture;
+    private Transformer transformer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnEnable()
+    {
+        gesture = GetComponent<TransformGesture>();
+        transformer = GetComponent<Transformer>();
+
+        transformer.enabled = false;
+        // Subscribe to gesture events
+        gesture.TransformStarted += transformStartedHandler;
+        gesture.TransformCompleted += transformCompletedHandler;
+    }
+
+    private void transformStartedHandler(object sender, EventArgs e)
+    {
+        transformer.enabled = true;
+    }
+
+    private void transformCompletedHandler(object sender, EventArgs e)
+    {
+        transformer.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
