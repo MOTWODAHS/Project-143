f65b6deb195ba3440bf90ea6eb3fa0dfacc6b8d9
MOTWODAHS <duxuhao521123@gmail.com>
2019-04-12 16:18:55 -0400

    button add

--- Assets/Scripts/StartScreen/StartScreenWordControl.cs
+++ Assets/Scripts/StartScreen/StartScreenWordControl.cs
@@ -4,23 +4,10 @@ using UnityEngine;
 using DG.Tweening;
 using UnityEngine.SceneManagement;
 
-public class StartScreenWordControl : TapableObject
+public class StartScreenWordControl : MonoBehaviour
 {
     void Start()
     {
         this.gameObject.GetComponent<SpriteRenderer>().DOColor(new Color (0,0,0,1f),5f).SetEase(Ease.Linear);
-        StartCoroutine(OpenCollider());
-    }
-
-    IEnumerator OpenCollider()
-    {
-        yield return new WaitForSeconds(5f);
-        this.gameObject.GetComponent<BoxCollider2D>().enabled = true;
-    }
-
-    public override void OnTap()
-    {
-        print("changeToMenu");
-        SceneManager.LoadScene("Menu");
     }
 }
--- Assets/Scripts/StartScreen/TrolleyController.cs
+++ Assets/Scripts/StartScreen/TrolleyController.cs
@@ -9,17 +9,22 @@ public class TrolleyController : MonoBehaviour
     public Transform ObjectToMove;
     private float distance;
 
+    public bool flag = false;
+
     void Update()
     {
-        //increase distance
-       distance += 1 * Time.deltaTime;
+        if(flag)
+        {
+            //increase distance
+            distance += 1 * Time.deltaTime;
 
-       //calculate position and tangent
-       Vector3 tangent;
-       ObjectToMove.position = GetComponent<BGCcMath>().CalcPositionAndTangentByDistance(distance, out tangent);
-       //this is a version for 3D. For 2D, comment this line and uncomment the next one
-       //ObjectToMove.rotation = Quaternion.LookRotation(tangent);
-       ObjectToMove.rotation = Quaternion.AngleAxis(Mathf.Atan2(tangent.y, tangent.x) * Mathf.Rad2Deg, Vector3.forward);
+            //calculate position and tangent
+            Vector3 tangent;
+            ObjectToMove.position = GetComponent<BGCcMath>().CalcPositionAndTangentByDistance(distance, out tangent);
+            //this is a version for 3D. For 2D, comment this line and uncomment the next one
+            //ObjectToMove.rotation = Quaternion.LookRotation(tangent);
+            ObjectToMove.rotation = Quaternion.AngleAxis(Mathf.Atan2(tangent.y, tangent.x) * Mathf.Rad2Deg, Vector3.forward);
+        }
 
     }
 }
