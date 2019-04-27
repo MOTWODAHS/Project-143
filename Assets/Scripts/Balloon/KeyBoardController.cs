390d7460ce6029612ca47c2491db94ec8b5b7e32
MOTWODAHS <duxuhao521123@gmail.com>
2019-04-27 16:39:58 -0400

    Change limitation of word

--- Assets/Scripts/Balloon/KeyBoardController.cs
+++ Assets/Scripts/Balloon/KeyBoardController.cs
@@ -4,10 +4,37 @@ using UnityEngine;
 
 public class KeyBoardController : MonoBehaviour
 {
+
+    public int interactionCode = 0;
     public string inputString;
+
+    [SerializeField]
+    private int wordLimit = 20;
+
+    void Start()
+    {
+        switch(interactionCode)
+        {
+            case 1:
+                wordLimit = 14;
+                break;
+            case 2:
+                wordLimit = 7;
+                break;
+            case 3:
+                wordLimit = 12;
+                break;
+            case 4:
+                wordLimit = 14;
+                break;
+            default:
+                wordLimit = 20;
+                break;
+        }
+    }
     public void AddInput(string rev_string)
     {
-        if(inputString.Length < 20)
+        if(inputString.Length < wordLimit)
         {
             if(string.Equals(rev_string,"Backspace"))
             {
