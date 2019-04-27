72434e24a1d53b6ba294d92b793ff1eda1c76081
MOTWODAHS <duxuhao521123@gmail.com>
2019-04-27 18:57:07 -0400

    Add sign function

---
+++ Assets/Scripts/HoldScreen/ArrowControl.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class ArrowControl : MonoBehaviour
+{
+    public Vector3 vec;
+
+    private Vector3 endPoint;
+
+    private Vector3 startPoint;
+    void Start()
+    {
+        startPoint = this.transform.position;
+        endPoint = startPoint + vec;
+        this.transform.DOMove(endPoint,1f).SetEase(Ease.Linear).SetLoops(-1,LoopType.Yoyo);
+    }
+
+}
